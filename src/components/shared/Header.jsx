1aff0f7d8051e45e27ecd02fc9cf52ba4d9bb3dd
esskeeper23 <rene2.martinez@outlook.com>
2022-11-21 12:02:38 -0800

    first commit

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <img className='header_logo' src="/header-icon.jpg" alt="" />
+    </header>
+  )
+}
+
+export default Header
